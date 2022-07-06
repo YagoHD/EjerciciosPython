9e481a3e0999631a253148dc254462651c586957
Yago Ramos Bermúdez <yagorb@hotmail.com>
2022-07-06 20:31:13 +0200

    ADD: EjerciciosFunciones [Parte 2]

--- PackFunciones/main.py
+++ PackFunciones/main.py
@@ -52,3 +52,74 @@ print(vocalConsonante("j"))
 # 5 - Escribir una función sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista.
 # Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
 
+def sum(lista):
+    total = 0
+    for numero in lista:
+       total += numero
+    return total
+
+print(sum([5,3,7]))
+
+def mult(lista):
+    total=1
+    for numero in lista:
+        total*= numero
+    return total
+
+print(mult([5,3,7]))
+
+# 6 - Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena "estoy probando"
+# debería devolver la cadena "odnaborp yotse"
+
+def inversa(palabra):
+    logitud=len(palabra)
+    invertida = palabra[logitud::-1]
+    return invertida
+
+print(inversa("Hola"))
+
+# 7 - Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el mismo aspecto
+# escritas invertidas), ejemplo: es_palindromo ("radar") tendría que devolver True.
+
+def es_palindromo(palabra):
+    palabraGirada = palabra[::-1]
+    if palabraGirada ==palabra:
+        return True
+    else:
+        return False
+
+print(es_palindromo("hooh"))
+
+# 8 - Definir una función superposicion() que tome dos listas y devuelva True si tienen al menos 1 miembro en común o
+# devuelva False de lo contrario. Escribir la función usando el bucle for anidado.
+
+
+print("Ejercicio 8")
+def superposicion (lista1, lista2):
+    for i in lista1:
+        for x in lista2:
+            if i == x:
+                return True
+            else:
+                return False
+
+
+
+print(superposicion([9,9,7],[2,9,1]))
+
+# 9 - Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n.
+# Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx".
+
+def generar_n_caracteres(numero, letra):
+    print(numero * letra)
+
+print(generar_n_caracteres(7,"Y"))
+
+# 10 - Definir un histograma procedimiento() que tome una lista de números enteros e imprima un histograma en la pantalla.
+# Ejemplo: procedimiento([4, 9, 7]) debería imprimir lo siguiente:
+
+def procedimiento(lista):
+    for i in lista:
+        print(i*"*")
+
+print(procedimiento([5,7,2]))
